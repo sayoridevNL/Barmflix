9373c5b17d06a6abfea04d1c7cd30b61d25c6bdf
Sayoridev <sayoridev2@gmail.com>
2026-02-08 22:18:10 +0100

    beta 1.0.2
    added movies and headers, changed the video player , and made movies in a list

--- script.js
+++ script.js
@@ -1,29 +1,25 @@
 const movieGrid = document.getElementById('movieGrid');
 
 fetch('movies.json')
-    .then(response => {
-        if (!response.ok) throw new Error("Failed to load movie data");
-        return response.json();
-    })
+    .then(res => res.json())
     .then(movies => {
         movies.forEach(movie => {
             const card = document.createElement('div');
             card.className = 'movie-card';
-            
             card.innerHTML = `
                 <img src="${movie.thumbnail}" alt="${movie.title}">
-                <h3>${movie.title}</h3>
+                <div class="card-content">
+                    <h3>${movie.title}</h3>
+                    <p>${movie.description}</p>
+                </div>
             `;
-            
             card.onclick = () => {
                 const query = `?title=${encodeURIComponent(movie.title)}&file=${encodeURIComponent(movie.videoUrl)}&desc=${encodeURIComponent(movie.description)}`;
                 window.location.href = 'video.html' + query;
             };
-
             movieGrid.appendChild(card);
         });
     })
-    .catch(error => {
-        console.error("Error:", error);
-        movieGrid.innerHTML = `<p>Error loading movies. Please ensure you are using a local server.</p>`;
+    .catch(err => {
+        movieGrid.innerHTML = "<p style='color:red;'>Please use a local server to load movies.</p>";
     });
